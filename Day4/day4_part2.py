9bcd9e08b28b6daf61b1d71cc7fd6faaf07a2b57
jshart <jshart@gmail.com>
2022-12-05 10:31:27 +0000

    completed day 4

---
+++ Day4/day4_part2.py
@@ -0,0 +1,60 @@
+
+import math
+
+
+# Using readlines()
+file1 = open('Day4/data/input.txt', 'r')
+Lines = file1.readlines()
+
+# This function checks for overlapping ranges between 2 pairs of numbers
+
+
+def totalOverLap(s1, e1, s2, e2):
+
+    # is range 1 (s1-e1) inside range 2 (s2-e2)
+    # start in range
+    if s1 >= s2 and s1 <= e2:
+        # end in range
+        if e1 >= s2 and e1 <= e2:
+            print("R1 in R2 {},{}-{},{} OL:{}".format(s1, e1, s2, e2, 1))
+            return 1
+
+    # is range 2 (s2-e2) inside range 1 (s1-e1)
+    # start in range
+    if s2 >= s1 and s2 <= e1:
+        # end in range
+        if e2 >= s1 and e2 <= e1:
+            print("R2 in R1 {},{}-{},{} OL:{}".format(s1, e1, s2, e2, 1))
+            return 1
+
+    print("No overlap {},{}-{},{} OL:{}".format(s1, e1, s2, e2, 0))
+    return 0
+
+
+def partialOverLap(s1, e1, s2, e2):
+    if (s1 >= s2 and s1 <= e2) or (e2 >= s1 and e2 <= e1):
+        print("R1 in R2 {},{}-{},{} OL:{}".format(s1, e1, s2, e2, 1))
+        return 1
+
+    if (s2 >= s1 and s2 <= e1) or (e1 >= s2 and e1 <= e2):
+        print("R1 in R2 {},{}-{},{} OL:{}".format(s1, e1, s2, e2, 1))
+        return 1
+
+    return 0
+
+runningTotal = 0
+overLapCount = 0
+result = 0
+for line in Lines:
+    line = line.strip()
+
+    # split line into an array by commas
+    sl = line.split(',')
+
+    # check the individual ranges to see if they completely overlap
+    result = partialOverLap(int(sl[0]), int(sl[1]), int(sl[2]), int(sl[3]))
+    # print("{},{}-{},{} OL:{}".format(sl[0], sl[1], sl[2], sl[3], result))
+    overLapCount += result
+
+print("Answer:{}".format(overLapCount))
+
