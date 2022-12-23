46b41495b049cc20544b2669d9669885437f0708
jshart <jshart@gmail.com>
2022-12-23 08:59:33 +0000

    tetris progress

--- Day17/day17.py
+++ Day17/day17.py
@@ -1,8 +1,51 @@
 
 # Importing the library
-from enum import Enum
+from enum import IntEnum
 import pygame
 from pygame.locals import *
+
+
+class Board:
+    def __init__(self, width, height, scale):
+        self.width = width
+        self.height = height
+        self.board = [[0 for x in range(width)] for y in range(height)]
+        self.left = 0
+        self.top = 0
+        self.scale = scale
+
+    def asciiBoard(self):
+        # print the board out
+        for row in self.board:
+            for cell in row:
+                if cell == 0:
+                    print('.', end='')
+                elif cell == 1:
+                    print('#', end='')
+                elif cell == 2:
+                    print('Z', end='')
+            print()
+
+    def draw(self, surface):
+        cellFilled = (255, 0, 0)
+        cellBorder = (125, 125, 125)
+
+        for y, row in enumerate(self.board):
+            for x, cell in enumerate(row):
+                if cell == 0:
+                    cellContent = (0, 0, 0)
+                elif cell == 1:
+                    cellContent = cellFilled
+                elif cell == 2:
+                    cellContent = cellFilled
+
+                pygame.draw.rect(surface, cellContent, pygame.Rect(
+                    x*self.scale, y*self.scale, self.scale, self.scale))
+                pygame.draw.rect(surface, cellBorder, pygame.Rect(
+                    x*self.scale, y*self.scale, self.scale, self.scale), width=1)
+
+    def addRow(self):
+        self.board.insert(0, [0 for x in range(self.width)])
 
 
 class JetPattern:
@@ -12,24 +55,26 @@ class JetPattern:
         print("JetPattern initiatlised with length:{}".format(len(self.ins)))
 
     def getIns(self):
-        i=self.ins[self.index]
+        i = self.ins[self.index]
         print("Ins [{}] at index:{}".format(i, self.index))
-        
+
         self.index += 1
         if self.index >= len(self.ins):
             print("--> Instructions wrapping")
-            self.index=0
+            self.index = 0
 
         return i
 
 
+class TType(IntEnum):
+    HLINE = 0
+    CROSS = 1
+    BL = 2
+    VLINE = 3
+    BOX = 4
+
+
 class Tetromino:
-    class TType(Enum):
-        HLINE=0
-        CROSS=1
-        BL=2
-        VLINE=3
-        BOX=4
 
     # hLine = [1, 1, 1, 1]
     # cross = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
@@ -38,28 +83,28 @@ class Tetromino:
     # box = [[1, 1], [1, 1]]
 
     def __init__(self, type):
-        self.type=type
+        self.type = type
 
     def getShape(self):
         match self.type:
-            case Tetromino.TType.HLINE:
+            case TType.HLINE:
                 return [[1, 1, 1, 1]]
-            case Tetromino.TType.CROSS:
+            case TType.CROSS:
                 return [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
-            case Tetromino.TType.BL:
+            case TType.BL:
                 return [[0, 0, 1], [0, 0, 1], [1, 1, 1]]
-            case Tetromino.TType.VLINE:
+            case TType.VLINE:
                 return [[1], [1], [1], [1]]
-            case Tetromino.TType.BOX:
+            case TType.BOX:
                 return [[1, 1], [1, 1]]
 
     def printShape(self):
         print(self.getShape())
 
     def printAsciiShape(self):
-        s=self.getShape()
-        y=len(s)
-        x=len(s[0])
+        s = self.getShape()
+        y = len(s)
+        x = len(s[0])
         for i in range(y):
             for j in range(x):
                 if (s[i][j] == 1):
@@ -68,87 +113,118 @@ class Tetromino:
                     print(".", end="")
             print()
 
-    def draw(self, scale):
-        c=(255, 255, 0)
-        # draw the sand
-        pygame.draw.rect(surface, c, pygame.Rect(
-            self.x*scale, self.y*scale, scale, scale))
+    def draw(self, surface, scale, xoffset, yoffset):
+        cellFilled = (0, 255, 0)
+        cellBorder = (125, 125, 125)
+
+        s = self.getShape()
+
+        for y, row in enumerate(s):
+            for x, cell in enumerate(row):
+                if cell == 0:
+                    cellContent = (0, 0, 0)
+                elif cell == 1:
+                    cellContent = cellFilled
+                elif cell == 2:
+                    cellContent = cellFilled
+
+                tx = x+xoffset
+                ty = y+yoffset
+                pygame.draw.rect(surface, cellContent, pygame.Rect(
+                    tx*scale, ty*scale, scale, scale))
+                pygame.draw.rect(surface, cellBorder, pygame.Rect(
+                    tx*scale, ty*scale, scale, scale), width=1)
 
 
-def drawMap(scale, xmax, ymax, m):
+nextTetromino = TType.HLINE
 
-    rockColor=(125, 125, 125)
-    sandColor=(255, 255, 0)
 
-    for x in range(xmax):
-        for y in range(ymax):
-            if (m[x][y] == 1):
-                # draw the rock
-                pygame.draw.rect(surface, rockColor, pygame.Rect(
-                    x*scale, y*scale, scale, scale))
-                pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(
-                    x*scale, y*scale, scale, scale), width=1)
-            elif (m[x][y] == 2):
-                pygame.draw.rect(surface, sandColor, pygame.Rect(
-                    x*scale, y*scale, scale, scale))
+def getNextTetromino():
+    global nextTetromino
+    t = nextTetromino
+
+    nextTetromino += 1
+    if nextTetromino > TType.BOX:
+        nextTetromino = TType.HLINE
+
+    return t
 
 
 # Initializing Pygame and the font handler
 pygame.init()
 pygame.font.init()
-myFont=pygame.font.SysFont('Arial', 10)
+myFont = pygame.font.SysFont('Arial', 10)
 
 
 #### START OF DATA LOADING ####
-file1=open('Day17/data/input_test.txt', 'r')
+file1 = open('Day17/data/input_test.txt', 'r')
 
 # read file input.txt into an array of strings
-lines=file1.readlines()
+lines = file1.readlines()
 
 # loop through each line
 for line in lines:
-    line=line.strip()
+    line = line.strip()
     print(line)
 
 
-x=600
-y=200
-map=[]
-
-shape=Tetromino(Tetromino.TType.HLINE)
-shape.printAsciiShape()
-shape=Tetromino(Tetromino.TType.CROSS)
-shape.printAsciiShape()
-shape=Tetromino(Tetromino.TType.BL)
-shape.printAsciiShape()
-shape=Tetromino(Tetromino.TType.VLINE)
-shape.printAsciiShape()
-shape=Tetromino(Tetromino.TType.BOX)
-shape.printAsciiShape()
-
-ins=JetPattern(lines[0])
+ins = JetPattern(lines[0])
 
 for c in range(len(lines[0])*2):
-    i=ins.getIns()
+    i = ins.getIns()
 
 
+x = 7
+y = 25
 # setup the display, now we know how big we need it
-scale=3
-surface=pygame.display.set_mode((scale*x, scale*y))
+scale = 20
+
+board = Board(x, y, scale)
+
+# test code
+board.addRow()
+board.asciiBoard()
+# end test code
+
+surface = pygame.display.set_mode((scale*x, scale*y))
 
 
-# surface.fill((0, 0, 0))
+surface.fill((0, 0, 0))
 
-# drawMap(scale, x, y, map)
-# pygame.display.flip()
+board.draw(surface)
+
+for c in range(5):
+    shape = Tetromino(getNextTetromino())
+    shape.draw(surface, board.scale, 2, c*5)
+
+pygame.display.flip()
+
+for c in range(5):
+    shape = Tetromino(getNextTetromino())
+    cycles = 0
+
+    while cycles < y:
+        cycles += 1
+
+        surface.fill((0, 0, 0))
+        board.draw(surface)
+        shape.draw(surface, board.scale, 2, cycles)
+        pygame.display.flip()
+
+        pygame.time.wait(200)
+
+print("done")
 
 
 pygame.event.clear()
 while True:
-    event=pygame.event.wait()
+    event = pygame.event.wait()
     if event.type == QUIT:
         pygame.quit()
         exit()
     elif event.type == KEYDOWN:
         if event.key == K_f:
             print("foo!")
+        if event.key == K_ESCAPE:
+            pygame.quit()
+            exit()
--- Day21/day21.py
+++ Day21/day21.py
@@ -1,110 +1,61 @@
-# for each sensor/beacon pair:
-# 1) calculate the Man-dist for sensor/beacon pair - call it M
-# 2) any sensor within distant M of target row Y can block some cells
-# 3) based on distance calculate impacted X values (impacted X values inversely proportional to Y distance)
-# 4) add impacted values to a *set*
-# 5) if either the sensor or beacon sit on the line also add them to the set
-# count elements in set
-# m-dist dealing with negatives - the distance is the ABS() of the X delta + the ABS() of the Y delta
-
-
-# used for parsing the raw data from the input file, this just splits the pairs into ints
-def getCoords(s):
-    parts = s.split(',')
-    return (int(parts[0]), int(parts[1]))
-
-
-# given a sensor and a beacon work out the manhattan distance between them
-def mDist(s, b):
-    x1, y1 = s
-    x2, y2 = b
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-# for a given sensor and the manhatten distance calculated based on the beacon it can detect
-# work out an area it can see and then determine if a given Y value is in that area
-def radiusOverlapsY(s, m, y):
-    x1, y1 = s
-    ys = y-m
-    ye = y+m
-    return ys <= y1 <= ye
-
-
-# for a given sensor and the manhatten distance calculated based on the beacon it can detect
-# work out the range of X values at a given Y value in that area and return them
-def impactedXValues(s, m, y):
-    x1, y1 = s
-    yOffset = abs(y1-y)
-    xs = x1-(m-yOffset)
-    xe = x1+(m-yOffset)
-    return list(range(xs, xe+1))
-
-
 #### START OF DATA LOADING ####
-file1 = open('Day15/data/input.txt', 'r')
+file1 = open('Day21/data/input.txt', 'r')
 
 # read file input.txt into an array of strings
-Lines = file1.readlines()
+lines = file1.readlines()
 
-sensors = []
-beacons = []
-mRadius = []
+resolved = dict()
+unresolved = []
 
 # loop through each line
-for line in Lines:
+for line in lines:
     line = line.strip()
-    parts = line.split(':')
-    sensor = parts[0]
-    beacon = parts[1]
-    sensors.append(getCoords(sensor))
-    beacons.append(getCoords(beacon))
+    print(line)
+    parts = line.split(' ')
 
-print("SENSORS:", end='')
-print(sensors)
-print("BEACONS:", end='')
-print(beacons)
+    if len(parts) == 2:
+        resolved[parts[0]] = int(parts[1])
+    elif len(parts) == 4:
+        unresolved.append(parts)
+
 #### DATA LOAD COMPLETE ####
 
 
-cl = 2000000
-coverage = set()
-# check each sensor in turn, work out the area it can see, and then determine for the given value (cl)
-# which X values are impacted (if any), append these to a set, so we can get a unique list of all impacted X values
-# across the coverage line (cl) based on all the beacons overlapping fields of view
-for c, v in enumerate(sensors):
-    mRadius.append(mDist(v, beacons[c]))
-    #print("{} sees {} at MDist {}".format(v, beacons[c], mRadius[c]), end='')
+print("Resolved items: ", end='')
+print(resolved)
 
-    if radiusOverlapsY(v, mRadius[c], cl):
-        #print(" overlaps Y=10 ", end='')
-        for x in impactedXValues(v, mRadius[c], cl):
-            coverage.add(x)
+print("Unresolved items: ", end='')
+print(unresolved)
 
-        #print(impactedXValues(v, mRadius[c], cl))
-    else:
-        #print(" no overlap with Y=10")
-        pass
+while len(unresolved) > 0:
+    print("iterating")
 
-#print("Coverage:", end='')
-# print(coverage)
-coverageLen = len(coverage)
-# print(coverageLen)
+    for i in unresolved:
 
-# some of the values we can "see" are actually beacons and not empty spaces, given the beacon list
-# is relatively small, its easier to simply check if an fall in the coverage line, and now reduce
-# the count than try to take these into account above and complicate the area code
-beaconsSeen = set()
-# lets check how many beacons we have in this area;
-for i in beacons:
-    if i[1] == cl:
-        if i[0] in coverage:
-            beaconsSeen.add(i)
+        # check to see if both of the vars this depends on are resolved
+        if i[1] in resolved and i[3] in resolved:
 
-#print("Beacons:", end = '')
-# print(beaconsSeen)
-beaconsSeenLen = len(beaconsSeen)
-# print(beaconsSeenLen)
+            # we should be able to calculate this now
+            match i[2]:
+                case '+':
+                    ans = resolved[i[1]] + resolved[i[3]]
+                    pass
+                case '*':
+                    ans = resolved[i[1]] * resolved[i[3]]
+                    pass
+                case '-':
+                    ans = resolved[i[1]] - resolved[i[3]]
+                    pass
+                case '/':
+                    ans = resolved[i[1]] / resolved[i[3]]
+                    pass
 
+            resolved[i[0]] = ans
+            unresolved.remove(i)
+            break
 
-finalAnswer = coverageLen-beaconsSeenLen
-print("Final Answer:{}".format(finalAnswer))
+print("Resolved items: ", end='')
+print(resolved)
+
+print("Unresolved items: ", end='')
+print(unresolved)
